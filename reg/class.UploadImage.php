2eb3c40fe4f62ce9ce8e6157c065a0d869f65f57
Tanapat Kahabodeekanokkul <tanapat_k@cmu.ac.th>
2015-02-15 14:59:58 +0700

    Update class.UploadImage.php, all: 57%. My last NC project.

--- reg/class.UploadImage.php
+++ reg/class.UploadImage.php
@@ -1,13 +1,61 @@
 <?php
 require_once 'config.inc.php';
 class UploadImageException extends Exception{
-	
-	
+	const
+			TYPE_UPLOAD_ERROR=1,
+			TYPE_IMG_ERROR=2,
+			
+			CODE_UNSUPPORT_EXT=1,
+			CODE_WRONG_FORMAT=2,
+			CODE_UNWRITTABLE=3;
+	public function __construct($filename='',$code=0,$type=0){
+		if($type==self::TYPE_IMG_ERROR){
+			$message="Fail to process ".$message." because ";
+			switch($code){
+				case self::CODE_UNSUPPORT_EXT:
+					$message.="uploaded file is not supported. Please upload JPEG (*.jpg, *.jpeg), PNG (*.png), or GIF (*.gif)  image only.";
+					break;
+				case self::CODE_WRONG_FORMAT:
+					$message.="upload file format does not match its filetype (extension). Please check your image whether it is damage.";
+					break;
+				case self::CODE_UNWRITTABLE:
+					$message.="the registration system can not save uploaded file on server. Please contact administrator.";
+					break;
+				default:
+			}
+		}elseif($type==self::TYPE_UPLOAD_ERROR){
+			$message.="Unable to upload file because ";
+			switch($code){
+				case UPLOAD_ERR_INI_SIZE:
+				case UPLOAD_ERR_FORM_SIZE:
+					$message.="uploaded filesize is too large. Please compress your image, change to other filetypes, or reduce its resolution.";
+					break;
+				case UPLOAD_ERR_PARTIAL:
+					$message.="some part of file was not uploaded. Please check your internet connection and try to upload the image again."
+					;break;
+				case UPLOAD_ERR_NO_FILE:
+					$message.="there was not uploaded file. Please check your internet connection and your web browser. After solving the problems, try to upload the image again.";
+					break;
+				case UPLOAD_ERR_NO_TMP_DIR:
+					$message.="temporary directory on web server is not found (Code: UPLOAD_ERR_NO_TMP_DIR). Please contact administrator.";
+					break;
+				case UPLOAD_ERR_CANT_WRITE:
+					$message.="the registration system can not write any file to the server (Code: UPLOAD_ERR_CANT_WRITE). Please contact administrator.";
+					break;
+				case UPLOAD_ERR_EXTENSION:
+					$message.="unknown problem occurs in the registration system (Code: UPLOAD_ERR_EXTENSION). Please contact administrator.";
+			}
+		}
+		parent::__construct($message,$code);
+	}
 }
 
 class UploadImageOriginal{ // Upload image and convert to jpg
 	const JPG='jpg';
-	public $team_id, $formName='uploadImgInput';
+	public $team_id, $formName='uploadImgInput',
+	$minFileSize=204800, $minResolutionArray=array(1754,1240), // A4 150dpi
+	$quality=25, $algorithmFit=true // true = Fit, false = FIll
+	;
 	
 	
 	
@@ -15,21 +63,87 @@ class UploadImageOriginal{ // Upload image and convert to jpg
 		$file=$_FILES[$this->formName];
 		
 		if($file['error']!=UPLOAD_ERR_OK)
-			;
+			throw new UploadImageException($file['name'], $file['error'], UploadImageException::TYPE_UPLOAD_ERROR);
 		// Dectect ext and creat img resource to Convert toJPG
+		$move=false;
+		$img=NULL;
 		switch($file['type']){
 			case 'image/pjpeg':
 			case 'image/jpeg':
 			case 'image/jpg':
+				if($file['size']<=$this->minFileSize) $move=true;
+				else $img=imagecreatefromjpeg($file['tmp_name']);
+				break;
+			case 'image/gif':
+				$img=imagecreatefromgif($file['tmp_name']);
+				break;
+			case 'image/png':
+				$img=imagecreatefrompng($file['tmp_name']);
+				break;
+			default:
+				throw new UploadImageException($file['name'], UploadImageException::CODE_UNSUPPORT_EXT, UploadImageException::TYPE_IMG_ERROR);
+				return false;
 		}
-		// Move if Small size
 		
-		// Compress
-		
-		// Export
-		
-		// destroy
-		
+		if($move){ // Move if Small size
+			if(move_uploaded_file($_FILES['tsp']['tmp_name'], $this->getFolder($filename))){
+				return true;
+			}else{
+				throw new UploadImageException($file['name'], UploadImageException::CODE_UNWRITTABLE, UploadImageException::TYPE_IMG_ERROR);
+				return false;
+			}
+		}elseif($img===false){ // If can not create img resource
+			throw new UploadImageException($file['name'], UploadImageException::CODE_WRONG_FORMAT, UploadImageException::TYPE_IMG_ERROR);
+			return false;
+		}else{
+			// set target resolution
+			$imgW=imagesx($img);
+			$imgH=imagesy($img);
+			
+			if($imgH>$imgW){
+				$h=max($this->minResolutionArray);
+				$w=min($this->minResolutionArray);
+			}else{
+				$h=min($this->minResolutionArray);
+				$w=max($this->minResolutionArray);
+			}
+			// Compress
+			if($h>=$imgH || $w>=$imgW || $h*$w>=$imgH*$imgW){ // Resolution <= minResolutionArray
+				$resize=$img;
+			}else{
+				// Algorithm: Fit max($imgH,$imgW) < max($h,$w) / Fill min($imgH,$imgW) < min($h,$w)
+				/*
+				// Algorithm is Fit.
+				if(($imgH*$w)>($h*$imgW)) // Equivalent to ($imgH/$imgW)>($h/$w) สูงกว่ากว้าง when size>=0
+					$w=round($h*$imgW/$imgH); // ใช้ $h เป็นหลัก
+				else
+					$h=round($w*$imgH/$imgW); // ใช้ $w เป็นหลัก
+				// Algorithm is Fill.
+				if(($imgH*$w)<=($h*$imgW)) // Equivalent to ($imgH/$imgW)>($h/$w) when size>=0
+					$w=round($h*$imgW/$imgH); // ใช้ $h เป็นหลัก
+				else
+					$h=round($w*$imgH/$imgW); // ใช้ $w เป็นหลัก
+				*/
+				$resize=($imgH*$w)>($h*$imgW);
+				if($this->algorithmFit?$resize:!$resize) // Equivalent to ($imgH/$imgW)>($h/$w) when size>=0
+					$w=round($h*$imgW/$imgH); // ใช้ $h เป็นหลัก
+				else
+					$h=round($w*$imgH/$imgW); // ใช้ $w เป็นหลัก
+				$resize=imagecreatetruecolor($w,$h);
+				imagecopyresampled($resize,$img,0,0,0,0,$w,$h,$imgW,$imgH);
+			}
+			// Export
+			if(!imagejpeg($resize,$this->getFolder($filename),$this->quality)){
+				throw new UploadImageException($file['name'], UploadImageException::CODE_UNWRITTABLE, UploadImageException::TYPE_IMG_ERROR);
+				return false;
+			}
+			// destroy
+			if(isset($img)) imagedestroy($img);
+			if(isset($resize)) imagedestroy($resize);
+			unset($img,$resize,$imgH,$imgW,$file,$filename,$h,$w,$move);
+			return true;
+		}
+		return false;
 	}
 	
 	// return <img>
